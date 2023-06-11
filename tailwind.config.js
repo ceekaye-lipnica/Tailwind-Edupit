c4d7e604e666261f7a9a0826f05b606524f6b298
Ceekaye Lipnica <ceekayelipnica04@gmail.com>
2023-06-12 00:29:52 +0800

    6/11/23

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
+      textFillColor: theme => theme('borderColor'),
+      textStrokeColor: theme => theme('borderColor'),
+      textStrokeWidth: theme => theme('borderWidth'),
+
+      paintOrder: {
+        'fsm': { paintOrder: 'fill stroke markers' },
+        'fms': { paintOrder: 'fill markers stroke' },
+        'sfm': { paintOrder: 'stroke fill markers' },
+        'smf': { paintOrder: 'stroke markers fill' },
+        'mfs': { paintOrder: 'markers fill stroke' },
+        'msf': { paintOrder: 'markers stroke fill' },
+      },
     extend: {
       colors: {
         light: '#ffffff',
@@ -14,10 +26,46 @@ module.exports = {
 
       backgroundImage: {
         'banner1': 'url("./img/slider-bg.jpg")',
+        'shape': 'url("./img/bg-shape.png")',
 
       },
+
+      boxShadow: {
+        'xl': '0px 0px 30px 0px rgb(0 0 0 / 3%)',
+      },
+
+      keyframes: {
+        sliding1:{
+          from: {
+            left:'0',
+          },
+          to: {
+            left:'100%',
+          },
+        },
+        sliding2:{
+          from: {
+            right:'0',
+          },
+          to: {
+            right:'100%',
+          },
+        }
+      },
+      animation:{
+        sliding1: 'sliding1 3s linear infinite',
+        sliding2: 'sliding2 3s linear infinite',
+      },
+    },
+    variants: { // all the following default to ['responsive']
+      textFillColor: ['responsive'],
+      textStrokeColor: ['responsive'],
+      textStrokeWidth: ['responsive'],
+      paintOrder: ['responsive'],
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-text-fill-stroke')(),
+  ],
 }
 
